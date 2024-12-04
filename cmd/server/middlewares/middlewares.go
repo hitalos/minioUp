cbfc85cd296128547bfb8a05785cab0bc3a430ed
Hítalo Silva <hitalos@gmail.com>
2024-12-04 13:08:38 -0300

    Added middleware "HasRole"

--- cmd/server/middlewares/middlewares.go
+++ cmd/server/middlewares/middlewares.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"net/http"
+	"slices"
 )
 
 func StripPrefix(prefix string) func(http.Handler) http.Handler {
@@ -23,6 +24,18 @@ func AllowedHosts(hosts ...string) func(http.Handler) http.Handler {
 					next.ServeHTTP(w, r)
 					return
 				}
+			}
+			w.WriteHeader(http.StatusForbidden)
+		})
+	}
+}
+
+func HasRole(role string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if slices.Contains(r.Header.Values("X-Roles"), role) {
+				next.ServeHTTP(w, r)
+				return
 			}
 			w.WriteHeader(http.StatusForbidden)
 		})
