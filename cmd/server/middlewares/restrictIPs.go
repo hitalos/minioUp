936ddd963b5b78d16475904c09b7c989ef26854a
Hítalo Silva <hitalos@gmail.com>
2024-12-04 13:15:56 -0300

    Added middleware to restricting requests by IP/NET

---
+++ cmd/server/middlewares/restrictIPs.go
@@ -0,0 +1,41 @@
+package middlewares
+
+import (
+	"fmt"
+	"log/slog"
+	"net"
+	"net/http"
+	"strings"
+)
+
+func RestrictIP(cidrs []string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		allowedIPs := map[string]string{"127.0.0.1": "", "[::1]": ""}
+		for _, ip := range cidrs {
+			addr := net.ParseIP(strings.TrimSpace(ip))
+			if addr != nil {
+				allowedIPs[addr.String()] = ""
+			}
+		}
+
+		for ip := range allowedIPs {
+			slog.Info(fmt.Sprintf("Restricting access from %s", ip))
+		}
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			parts := strings.Split(r.RemoteAddr, ":")
+			remoteIP := strings.Join(parts[0:len(parts)-1], ":")
+
+			for ip := range allowedIPs {
+				if ip == remoteIP {
+					next.ServeHTTP(w, r)
+					return
+				}
+			}
+
+			w.WriteHeader(http.StatusForbidden)
+			_, _ = w.Write([]byte("Access Forbidden"))
+			slog.Info(fmt.Sprintf("Access Forbidden for remote client %s", r.RemoteAddr))
+		})
+	}
+}
